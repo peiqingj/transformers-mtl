2c831facf0cefbe628a0b748d5a65cdcb1830e9b
Jiang, Peiqing <peiqing.jiang@intel.com>
2023-12-13 09:58:27 +0800

    use streaming mode

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
 # Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from transformers.models.llama.modeling_llama import LLAMA_ATTENTION_CLASSES
+import transformers
 
-from modeling_llama import LlamaAttentionNPU
-LLAMA_ATTENTION_CLASSES["LlamaAttentionNPU"] = LlamaAttentionNPU
-# tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-print(model.name)
+
+streamer=transformers.TextStreamer(None)
+p = transformers.pipeline(
+    'text-generation',
+    model='meta-llama/Llama-2-7b-chat-hf',
+    tokenizer='meta-llama/Llama-2-7b-chat-hf',
+    streamer=streamer
+)
+streamer.tokenizer = p.tokenizer
+sequences = p(
+    'what is the recipe of mayonnaise?',
+    temperature=0.9,
+    top_k=50,
+    top_p=0.9,
+    max_length=500,
+)
