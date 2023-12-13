aab32039217092d422b2c6a97f04e9577c948618
Jiang, Peiqing <peiqing.jiang@intel.com>
2023-12-13 10:26:30 +0800

    fix hacking

--- run.py
+++ run.py
@@ -2,16 +2,21 @@
 import transformers
 from modeling_llama import LlamaAttentionNPU
 
-
-transformers.models.llama.modeling_llama.LlamaAttention = LlamaAttentionNPU  # hacking LlamaAttention
+# hacking eager LlamaAttention by LlamaAttentionNPU
+mode = 'eager'
+transformers.models.llama.modeling_llama.LLAMA_ATTENTION_CLASSES[mode] = LlamaAttentionNPU  
 streamer=transformers.TextStreamer(None)
 p = transformers.pipeline(
     'text-generation',
-    model='meta-llama/Llama-2-7b-chat-hf',
+    model=transformers.LlamaForCausalLM.from_pretrained('meta-llama/Llama-2-7b-chat-hf', attn_implementation=mode),
     tokenizer='meta-llama/Llama-2-7b-chat-hf',
-    streamer=streamer
+    streamer=streamer,
 )
 streamer.tokenizer = p.tokenizer
+
+# verify the hacking is loaded
+assert id(p.model.model.layers[0].self_attn.__class__) == id(LlamaAttentionNPU), 'hacking failed'
+
 sequences = p(
     'what is the recipe of mayonnaise?',
     temperature=0.9,
